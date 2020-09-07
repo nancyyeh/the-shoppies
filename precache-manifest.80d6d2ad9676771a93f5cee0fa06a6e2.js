56d0994888477686680300c27130b4e68ac4ea7b
Nancy Yeh <heyycnan@gmail.com>
2020-09-07 04:53:27 -0700

    Updates

---
+++ precache-manifest.80d6d2ad9676771a93f5cee0fa06a6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04760fce32efdd05c20864d73dc1457",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "df3042f669fc613994f5",
+    "url": "/the-shoppies/static/css/main.381c446d.chunk.css"
+  },
+  {
+    "revision": "a1e85899dcc67190f630",
+    "url": "/the-shoppies/static/js/2.66f74a4e.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/the-shoppies/static/js/2.66f74a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3042f669fc613994f5",
+    "url": "/the-shoppies/static/js/main.470cc07d.chunk.js"
+  },
+  {
+    "revision": "7f717e5177a62183cc17",
+    "url": "/the-shoppies/static/js/runtime-main.71ad18d3.js"
+  }
+]);
